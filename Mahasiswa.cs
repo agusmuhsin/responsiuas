fe2c936bf3c366e8dffe58c7dda4155edf9e999b
agus muhsin <muhammadagusmuhsin.tkj1@gmail.com>
2021-07-13 14:58:56 +0700

    uas

---
+++ Mahasiswa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectPenjualan
+{
+    public class Mahasiswa
+    {
+        // PERINTAH: lengkapi property class mahasiswa, sesuai petunjuk soal
+       
+        public string Nota { get; set; }
+        public string Tanggal { get; set; }
+
+        public string Costumer { get; set; }
+
+        public string Jenis { get; set; }
+
+        public double Total { get; set; }
+
+    }
+}
